699f4ce47fd634bb47832c847ab52e0bbc27f5ad
Usman Bin Tariq <f223662@cfd.nu.edu.pk>
2024-10-11 10:41:26 +0500

    First

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+
+   
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+   
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+    
+    static void main(String []args)
+    {
+    	
+    }
+}
